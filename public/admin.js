c32f9d81db2256c5525d35eb1b4ebf06ef5d68a3
Richard Quizon <codenomics@proton.me>
2023-05-15 06:34:35 -0700

    finished assignment and made a few updates

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,41 @@
-
 // Your Code Here
+
+// Displays the list of books
+fetch('http://localhost:3001/listBooks')
+    .then(response => response.json())
+    .then(books => {
+        const bookList = document.querySelector('#root'); // Found that I needed to access the <div id="root"></div> to see the bookList
+
+        books.forEach(book => {
+            const div = document.createElement('div');
+            const title = document.createElement('h2');
+        title.textContent = book.title;
+        const input = document.createElement('input');
+        input.type = 'number';
+        input.value = book.quantity;
+        const button = document.createElement('button');
+        button.textContent = 'Save';
+        button.addEventListener('click', () => {
+            // fetch(`http://localhost:3001/updateBook` ,{ 
+            // Had trouble connecting to the server.js and the /admin would not render on the browser.
+            fetch(`http://localhost:3001/updateBook/${book.id}` ,{
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ id: book.id, quantity: input.value })
+             })
+                .then(response => response.json())
+                .then(updateBook => {
+                    input.value = updateBook.quantity;
+                })
+                .catch(error => console.log(error));
+         });
+        bookList.appendChild(title);
+        bookList.appendChild(input);
+        bookList.appendChild(button);
+        bookList.appendChild(div);
+    })  
+})
+
+.catch(error => console.error(error));
--- server.js
+++ server.js
@@ -11,17 +11,37 @@ async function main() {
     app.use(cors());
 
     app.use(bodyParser.json());
+    // When going on the 3001/admin, saw in the console a MIME type error. Looked on google and found an explanation that explained how server.js is not serving statc files and setting their MIME type. added this 'express.static to fix this issue.
+    app.use(express.static('public'));
 
     app.get('/listBooks', async (req, res) => {
         let books = await loadBooks()
         res.json(books);
     })
 
-    app.patch('/updateBook', async (req, res) => {
+    app.get('/admin', async (req, res) => {
+        res.sendFile(__dirname + '/public/admin.html');
+      });
+      
+
+    // app.patch('/updateBook', async (req, res) => {
+    //     let books = await loadBooks()
+    //     if (!req.body.id) return res.status(400).json({ error: true, message: `'id' is required in the request body when calling 'updateBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.` })
+    //     let book = books.find(book => book.id === req.body.id);
+    //     if (!book) return res.status(404).json({ error: true, message: `Could not find a book with an id of ${req.body.id}` })
+    //     const { title, year, quantity, imageURL, description } = { ...book, ...req.body };
+    //     Object.assign(book, { title, year, quantity, imageURL, description });
+    //     await saveBooks(books)
+    //     res.json(book)
+    // })
+
+    // tried out a different variation of the app.patch. Saw that the app delete was referercing an 'id' and using a 'req.params.id', so I figured it may be the same for the patch. Made the changes and now the data will update.
+    app.patch('/updateBook/:id', async (req, res) => {
         let books = await loadBooks()
-        if (!req.body.id) return res.status(400).json({ error: true, message: `'id' is required in the request body when calling 'updateBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.` })
-        let book = books.find(book => book.id === req.body.id);
-        if (!book) return res.status(404).json({ error: true, message: `Could not find a book with an id of ${req.body.id}` })
+        let id = parseInt(req.params.id);
+        if (!id) return res.status(400).json({ error: true, message: `'id' is required in the request body when calling 'updateBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.` })
+        let book = books.find(book => book.id === id);
+        if (!book) return res.status(404).json({ error: true, message: `Could not find a book with an id of ${id}` })
         const { title, year, quantity, imageURL, description } = { ...book, ...req.body };
         Object.assign(book, { title, year, quantity, imageURL, description });
         await saveBooks(books)
